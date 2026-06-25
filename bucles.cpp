3d7e2b51b075ed621c65fb8f6be2dd0a9a82d37d
Leonardo <leofatari@gmail.com>
2026-06-25 17:51:06 -0500

    fix bucles

---
+++ bucles.cpp
@@ -0,0 +1,18 @@
+// un bucle es una estructura de control que repite un bloque de código varias veces de forma automática
+// Este siempre debe terner una terminacion para impedir la sobre carga del sistema
+
+// Este sirve para para ejecutar un bloque de codigo varias veces de forma automatica
+// Hasta que cumpla con una condicion especifica
+
+//Ejemplo de bucle
+
+int main  () {
+    
+    for (int i = 1; i <= 5; i++) {
+
+        cout << i <<: " " ;
+    }
+
+    return 0;
+
+}
