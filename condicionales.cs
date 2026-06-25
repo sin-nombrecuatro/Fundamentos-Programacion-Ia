5b07ce37836d2b4dc6466a1845300ca88826a4f5
Leonardo <leofatari@gmail.com>
2026-06-25 17:50:25 -0500

    fix condicionales

---
+++ condicionales.cs
@@ -0,0 +1,13 @@
+// Un condicional es instrucción que permite a un programa tomar decisiones y elegir qué camino seguir, dependiendo de si se cumple o no una regla específica
+
+// Estospermiten a un programa tomar decisiones y cambiar su camino de ejecución según si una condición es verdadera o falsa
+
+//Ejemplo
+
+if (key.input.down("space")) {
+    // Ejecutar acción si se presiona la barra espaciadora
+    jump();
+} else {
+    // Ejecutar acción si no se presiona la barra espaciadora
+    walk();
+}
