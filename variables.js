ce4616589e74b0c6848c5d376add63cff1784b47
Leonardo <leofatari@gmail.com>
2026-06-25 17:52:19 -0500

    fix Variables

--- variables.js
+++ variables.js
@@ -2,5 +2,5 @@ let nombre = "Juan";
 let edad = 30;
 let esEstudiante = true;    
 
-alert("Las variables declaradas son son aquellas que almacenan información que puede cambiar durante\n la ejecución del programa");
-alert ("estas sirven para almacenar datos que pueden ser utilizados y modificados a lo largo del tiempo, como el nombre, la edad y el estado de estudiante de una persona.");
+//Las variables declaradas son son aquellas que almacenan información que puede cambiar durante la ejecución del programa
+//estas sirven para almacenar datos que pueden ser utilizados y modificados a lo largo del tiempo, como el nombre, la edad y el estado de estudiante de una persona.
